db4f2c3610dd274da9b8cb1981add28c871628cd
Andrew <84246113+arstamm@users.noreply.github.com>
2025-02-21 17:22:57 -0700

    Revert "Delete all fragments"
    This reverts commit b06f8fb5a34d083d1dca82869e6565f0542d4eae.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.material3.android)
     implementation(libs.androidx.storage)
     implementation(libs.androidx.storage)
-    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
