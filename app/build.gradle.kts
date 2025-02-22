b1a6890a7e1277b75d3222c938422b25b4e90866
Andrew <84246113+arstamm@users.noreply.github.com>
2025-02-21 17:33:29 -0700

    Delete Old UI Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.storage)
     implementation(libs.androidx.storage)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.activity.compose) // Required for setContent
